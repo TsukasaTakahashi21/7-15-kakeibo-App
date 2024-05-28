c40be730aefe7432081c5e680f2f0b9e8abe6089
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-05-29 00:17:26 +0900

    feat: 収入源一覧ページ

---
+++ src/public/incomes/income_sources/delete.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$dbUserName = 'root';
+$dbPassword = 'password';
+$pdo = new PDO(
+    'mysql:host=mysql;dbname=kakeibo;charset=utf8',
+    $dbUserName,
+    $dbPassword
+);
+
+if (!isset($_GET['id'])) {
+  header('Location: ./index.php');
+  exit();
+}
+
+$id = $_GET['id'];
+
+$sql = 'DELETE FROM income_sources WHERE id = :id';
+$statement = $pdo->prepare($sql);
+$statement->bindValue(':id', $id, PDO::PARAM_INT);
+$statement->execute();
+
+header('Location: ./index.php');
+exit();
+?>f
